0501978be9f7164c4cfe2bda8c640474c95eb70e
andeananda <dearizkyananda1@gmail.com>
2026-06-03 20:48:54 +0700

    dari awal

---
+++ service-distribusi/src/routes/shipmentRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const shipmentsController = require('../controllers/shipmentsController');
+
+/**
+ * @openapi
+ * /api/distribusi:
+ * get:
+ * summary: Ambil semua data pengiriman (Data agregat dari Dapur, Menu, & Sekolah)
+ * tags: [Distribusi]
+ * post:
+ * summary: Buat jadwal pengiriman baru
+ * tags: [Distribusi]
+ */
+router.get('/', shipmentsController.getAllShipments);
+router.get('/:id', shipmentsController.getShipmentById);
+router.post('/', shipmentsController.createShipment);
+
+/**
+ * @openapi
+ * /api/distribusi/{id}:
+ * put:
+ * summary: Update status pengiriman & waktu sampai otomatis
+ * tags: [Distribusi]
+ * delete:
+ * summary: Hapus riwayat pengiriman
+ * tags: [Distribusi]
+ */
+router.put('/:id', shipmentsController.updateShipment);
+router.delete('/:id', shipmentsController.deleteShipment);
+
+module.exports = router;
